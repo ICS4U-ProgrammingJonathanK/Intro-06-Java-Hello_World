bf30dc9fc2229606ccfe16d45a5eaf4a5598062e
Ubuntu <ubuntu@ip-172-31-88-28.ec2.internal>
2022-02-17 00:17:13 +0000

    Created HelloWorld w/ style checking

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+
+* The HelloWorld program implements an application that
+
+* simply displays "Hello World!" to the standard output.
+
+*
+
+* @author  Jonathan Kene
+
+* @version 1.0
+
+* @since   2022-02-16
+
+*/
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
